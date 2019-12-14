58fe86a2257c8acf58f5cab441c6dbdc25709a57
wweebbddeevv <wweebbddeevv@web.de>
2019-12-14 23:01:35 +0100

    Change 'architecture'

---
+++ setupPage.js
@@ -0,0 +1,25 @@
+function add(row, col) {
+    var element = document.createElement("input");
+    element.setAttribute("type", "button");
+    element.setAttribute("value", "");
+    var buttonName =  "button" + row + col;
+    element.setAttribute("name", buttonName);
+    var funcCall = "clicked(" +  row + ", " + col + ")";
+    element.setAttribute("onclick", funcCall);
+    var docID = "div"+row+col;
+    var currentTD = document.getElementById(docID);
+    currentTD.appendChild(element);
+}
+
+function addButtonsToTable()
+{
+    add(0, 0);
+    add(0, 1);
+    add(0, 2);
+    add(1, 0);
+    add(1, 1);
+    add(1, 2);
+    add(2, 0);
+    add(2, 1);
+    add(2, 2);
+}
